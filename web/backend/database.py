744b2e3eda1ed26c0b9f9f5e983f0542767e4385
giosepnguyen <thientan2010.11@gmail.com>
2026-09-25 10:52:28 +0700

    Update database.py

--- web/backend/database.py
+++ web/backend/database.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Đọc thông tin từ file .env
 load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
@@ -12,22 +11,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-# Chuỗi kết nối tới MySQL
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-# Tạo "engine" - cầu nối chính tới database
-engine = create_engine(DATABASE_URL)
-
-# Tạo phiên làm việc (session) để truy vấn dữ liệu
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base để các "model" (bảng) sau này kế thừa
-Base = declarative_base()
-
-# Hàm này dùng để lấy 1 kết nối DB mỗi khi cần
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+database_url = URL.create(
+    drivername="mysql+pymysql",
+    username=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=int(DB_PORT) if DB_PORT else None,
+    database
